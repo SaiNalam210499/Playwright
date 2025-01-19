98d84568a088409618cd5160ef3fb90017f4140b
SaiNalam210499 <kalyansai2104@gmail.com>
2025-01-19 12:38:45 +0530

    mod

--- tests/webpage.java
+++ tests/webpage.java
@@ -1,12 +1,2 @@
-import ava.
-
-
-public class webpage {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = nextLine();
-        System.out.println("Hello " + name);
-    }
-}
+name=raj;
  
